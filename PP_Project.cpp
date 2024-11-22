72de27166dc1bd48bbbce24c8b7ed77adfb6a894
DenisAstashkin <ast.den27@gmail.com>
2024-11-22 16:14:09 +0400

    Nothing

--- PP_Project.cpp
+++ PP_Project.cpp
@@ -1,7 +1,6 @@
 ﻿#include "lib/interface_design.h"
 #include "lib/logic.h"
 
-#include <random>
 #include <iostream>
 
 using namespace std;
@@ -10,23 +9,7 @@ int main()
 {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
-	vector<vector<int>> v;
-	std::random_device dev;
-	std::mt19937 rng(dev());
-	std::uniform_int_distribution<std::mt19937::result_type> dist6(0, 2); // distribution in range [1, 6]
 
-	for (int i = 0; i < 9; i++)
-	{
-		vector<int> tmp;
-		for (int j = 0; j < i * 4; j++)
-		{
-			tmp.push_back(dist6(rng));
-		}
-		v.push_back(tmp);
-		tmp.clear();
-	}
-	
-	print_cinemahall(v);
 	system("pause");
 	return 0;
 }
