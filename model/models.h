6bd6d10fd32976b3aa9fd9c5fea1d2aa622fbb9f
Muxammed <muxammed.axmedov.07@mail.ru>
2024-11-24 18:22:58 +0400

    add Ticket struct

---
+++ model/models.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+struct Ticket {
+	std::string date;
+	std::string filmName;
+	std::string startTime;
+	int hallNumber;
+	std::vector<int> raws;
+	std::vector<int> seats;
+};
