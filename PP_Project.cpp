3c458e57437587664a160c077cf2298876fec2e7
DenisAstashkin <ast.den27@gmail.com>
2024-12-11 11:40:04 +0400

    Update main

--- PP_Project.cpp
+++ PP_Project.cpp
@@ -1,9 +1,24 @@
-﻿#include "lib/interface_design.h"
+﻿#ifdef _WIN32
+	#include <Windows.h>
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+#endif
+
+#include "lib/interface_design.h"
 #include "lib/logic.h"
+<<<<<<< HEAD
 #include "lib/registration.h"
 #include <iostream>
 <<<<<<< HEAD
 #include <string>
+=======
+#include "model/Film.h"
+
+#include <iostream>
+#include <stdio.h>
+#include <vector>
+#include <map>
+>>>>>>> b32c1b7 (Update main)
 
 using namespace std;
 
@@ -16,54 +31,78 @@ using namespace std;
 >>>>>>> f3b90d061529a7abfaa6a64184d4eeb259d54055
 int main()
 {
+<<<<<<< HEAD
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 <<<<<<< HEAD
 	registrate("loginsdada ", "passwordsdadas", "base.txt");
 	
 =======
+=======
+	
+	map<string, Film> seanses = get_file_info("/home/shadow_usr/C++/PP_Project/Data/Seanses.txt"); 	// пример строки в файле 01.12.2024;Doctor Who;11:00,12:00,16:00
+	map<string, Film> coasts = get_file_info("/home/shadow_usr/C++/PP_Project/Data/Coasts.txt");		// пример строки в файле 01.12.2024;Doctor Who;00:00-390rub,15:15-450rub,20:20-500rub
+>>>>>>> b32c1b7 (Update main)
 
-	auto seanses = get_file_info("путь до файла с сеансами"); 	// пример строки в файле 01.12.2024;Doctor Who;11:00,12:00,16:00
-	auto coasts = get_file_info("путь до файла с ценами");		// пример строки в файле 01.12.2024;Doctor Who;00:00-390rub,15:15-450rub,20:20-500rub
+	int choice = 1;
 
-	for (auto i : seanses)
+	center_print("1. Вход");
+	center_print("2. Регистрация");
+
+	cin >> choice;
+
+	switch (choice)
 	{
-		cout << i.first << '\n';
-		cout << i.second.name << ": ";
+	case 1:
+		
+		break;
+	case 2:
 
-		vector<pair<string, string>> coasts_film;
-
-		for (auto j : coasts)
-		{
-			if (i.second.name == j.second.name)
-			{
-				for (int h = 0; h < j.second.times.size(); h++)
-				{
-					vector<string>time_coast = split(j.second.times[h], "-");
-					coasts_film.push_back(make_pair(time_coast[0], time_coast[1]));
-				}
-				break;
-			}
-		}
-
-		for (int j = 0; j < i.second.times.size(); j++)
-		{
-			string coast;
-			for (int h = 0; h < coasts_film.size(); h++)
-			{
-				if (fulltime(i.second.times[j]) > fulltime(coasts_film[h].first))
-				{
-					coast = coasts_film[h].second;
-				}
-			}
-			cout << i.second.times[j] << " - " << coast << '\n';
-		}
-		cout << "\n\n";
+		break;
+	default:
+		break;
 	}
 
-	
+	// for (auto i : seanses)
+	// {
+	// 	cout << i.first << '\n';
+	// 	cout << i.second.name << ":\n";
 
+<<<<<<< HEAD
 >>>>>>> f3b90d061529a7abfaa6a64184d4eeb259d54055
 	system("pause");
+=======
+	// 	vector<pair<string, string>> coasts_film;
+
+	// 	for (auto j : coasts)
+	// 	{
+	// 		if (i.second.name == j.second.name)
+	// 		{
+	// 			for (int h = 0; h < j.second.data.size(); h++)
+	// 			{
+	// 				vector<string>time_coast = split(j.second.data[h], "-");
+	// 				coasts_film.push_back(make_pair(time_coast[0], time_coast[1]));
+	// 			}
+	// 			break;
+	// 		}
+	// 	}
+
+	// 	for (int j = 0; j < i.second.data.size(); j++)
+	// 	{
+	// 		string coast;
+	// 		for (int h = 0; h < coasts_film.size(); h++)
+	// 		{
+	// 			if (fulltime(i.second.data[j]) > fulltime(coasts_film[h].first))
+	// 			{
+	// 				coast = coasts_film[h].second;
+	// 			}
+	// 		}
+	// 		cout << i.second.data[j] << " - " << coast << '\n';
+	// 	}
+	// 	cout << "\n\n";
+	// }
+
+	getchar();
+>>>>>>> b32c1b7 (Update main)
 	return 0;
 }
